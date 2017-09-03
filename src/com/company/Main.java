d6a690fdb7daa36472b0554d6b7c5400045d5b96
mixa4634 <mixa4634@gmail.com>
2017-09-03 20:33:58 +0300

    Style change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ import java.util.Date;
 public class Main
 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println(new Date());
     }
 }
